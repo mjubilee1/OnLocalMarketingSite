622541d3a1dd2e0223137f90afcc2ea7d7447149
mjubilee1 <jubileeinvestmentsdmv@gmail.com>
2026-08-02 16:45:36 -0400

    update next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/OnLocalMarketingSite",
+  assetPrefix: "/OnLocalMarketingSite/",
   images: {
     unoptimized: true
   }
